8fab453534a89b024bc203153cc1d1b41c8f66a5
Monis Khan <mkhan@redhat.com>
2018-02-25 21:19:52 -0500

    global tests
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- staging/src/k8s.io/apimachinery/pkg/util/runtime/runtime_test.go
+++ staging/src/k8s.io/apimachinery/pkg/util/runtime/runtime_test.go
@@ -18,7 +18,10 @@ package runtime
 
 import (
 	"fmt"
+	"sync"
 	"testing"
+
+	"github.com/golang/groupcache/lru"
 )
 
 func TestHandleCrash(t *testing.T) {
@@ -67,5 +70,102 @@ func TestCustomHandleError(t *testing.T) {
 	HandleError(err)
 	if result != err {
 		t.Errorf("did not receive custom handler")
+	}
+}
+
+func TestGlobalDedupingErrorHandlerChangeSimilar(t *testing.T) {
+	oldCount := DedupingErrorHandler.count
+	oldCache := DedupingErrorHandler.cache
+	oldSimilar := DedupingErrorHandler.Similar
+	defer func() {
+		DedupingErrorHandler.count = oldCount
+		DedupingErrorHandler.cache = oldCache
+		DedupingErrorHandler.Similar = oldSimilar
+	}()
+
+	// temporarily reset any global state
+	resetGlobalDedupingErrorHandler()
+
+	errs := []error{
+		fmt.Errorf("test1"),
+		fmt.Errorf("test2"),
+		fmt.Errorf("test3"),
+	}
+
+	// with the default similar of 1, these are all considered different errors
+	// we must do this in a for loop otherwise the errors will have a different stack
+	for _, err := range errs {
+		HandleError(err)
+	}
+
+	checkCount(t, DedupingErrorHandler.count, 3, 1)
+
+	// reset again so we can see the effects of changing similar
+	resetGlobalDedupingErrorHandler()
+
+	// enable fuzzy matching
+	DedupingErrorHandler.Similar = 0.75
+
+	// now these should all be considered the same error
+	for _, err := range errs {
+		HandleError(err)
+	}
+
+	checkCount(t, DedupingErrorHandler.count, 1, 3)
+}
+
+func TestGlobalDedupingErrorHandlerGoroutineSafe(t *testing.T) {
+	oldCount := DedupingErrorHandler.count
+	oldCache := DedupingErrorHandler.cache
+	oldSimilar := DedupingErrorHandler.Similar
+	defer func() {
+		DedupingErrorHandler.count = oldCount
+		DedupingErrorHandler.cache = oldCache
+		DedupingErrorHandler.Similar = oldSimilar
+	}()
+
+	// temporarily reset any global state
+	resetGlobalDedupingErrorHandler()
+
+	// use untyped ints to make comparisons easier
+	const (
+		uniqueErrs   = 50
+		errFrequency = 70
+	)
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < errFrequency; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < uniqueErrs; j++ {
+				HandleError(fmt.Errorf("test%d", j))
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	checkCount(t, DedupingErrorHandler.count, uniqueErrs, errFrequency)
+}
+
+func checkCount(t *testing.T, count map[errKey]errVal, unique int, frequency uint64) {
+	t.Helper()
+
+	if length := len(count); length != unique {
+		t.Errorf("expected length %d got %d", unique, length)
+	}
+
+	for key, val := range count {
+		if val.count != frequency {
+			t.Errorf("expected count of %d for key=%v val=%v", frequency, key, val)
+		}
+	}
+}
+
+func resetGlobalDedupingErrorHandler() {
+	DedupingErrorHandler.count = make(map[errKey]errVal)
+	DedupingErrorHandler.cache = lru.New(cacheSize)
+	DedupingErrorHandler.cache.OnEvicted = func(key lru.Key, _ interface{}) {
+		delete(DedupingErrorHandler.count, key.(errKey))
 	}
 }
