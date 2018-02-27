b4257e831f735ee105a8605ea4584583b4ee32b2
Monis Khan <mkhan@redhat.com>
2018-02-27 17:01:37 -0500

    wip tests 2
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- staging/src/k8s.io/apimachinery/pkg/util/runtime/runtime_test.go
+++ staging/src/k8s.io/apimachinery/pkg/util/runtime/runtime_test.go
@@ -255,7 +255,6 @@ func (s stringErr) Error() string {
 }
 
 type logErrTracker struct {
-	t      *testing.T
 	called int
 }
 
@@ -263,66 +262,25 @@ func (l *logErrTracker) logErr(err error, count uint64) {
 	l.called++
 }
 
-func (l *logErrTracker) check(called int) {
-	l.t.Helper()
-	if l.called != called {
-		l.t.Errorf("expected %d got %d", called, l.called)
-	}
+type dedupingErrorHandlerTestRunner struct {
+	t       *testing.T
+	handler *dedupingErrorHandler
+	tracker *logErrTracker
 }
 
-func TestDedupingErrorHandlerDirectMatch(t *testing.T) {
-	for _, tc := range []struct {
-		name  string
-		check func(t *testing.T, handler *dedupingErrorHandler, tracker *logErrTracker)
-	}{
-		{
-			name: "simple unique errors",
-			check: func(t *testing.T, handler *dedupingErrorHandler, tracker *logErrTracker) {
-				err1 := fmt.Errorf("1")
-				err2 := fmt.Errorf("2")
-				err3 := fmt.Errorf("3")
+func (tr *dedupingErrorHandlerTestRunner) checkState(expectedCount map[error]int, expectedCache int, expectedLog int) {
+	tr.t.Helper()
 
-				handler.handleErr(err1)
-				handler.handleErr(err2)
-				handler.handleErr(err3)
-
-				checkCount(t, handler, map[error]int{
-					err1: 1,
-					err2: 1,
-					err3: 1,
-				})
-				checkCache(t, handler, 3)
-				tracker.check(3)
-			},
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			// setup a test version of dedupingErrorHandler
-			d := newDedupingErrorHandler(10, 0, delta, 1)
-			d.clock = clock.NewFakeClock(time.Time{})
-			l := &logErrTracker{t: t}
-			d.logErrorHandler = l.logErr
-			d.getStackHandler = func() (stack string) { return "" } // make all errors have the same stack
-
-			// run the test and check results
-			tc.check(t, d, l)
-		})
-	}
+	tr.checkCount(expectedCount)
+	tr.checkCache(expectedCache)
+	tr.checkLog(expectedLog)
 }
 
-func TestDedupingErrorHandlerFuzzyMatch(t *testing.T) {
-	// TODO
-}
+func (tr *dedupingErrorHandlerTestRunner) checkCount(expected map[error]int) {
+	tr.t.Helper()
 
-func TestDedupingErrorHandlerGetStack(t *testing.T) {
-	// TODO
-}
-
-func checkCount(t *testing.T, handler *dedupingErrorHandler, expected map[error]int) {
-	t.Helper()
-
-	if len(handler.count) != len(expected) {
-		t.Errorf("length mismatch %v %v", handler.count, expected)
+	if len(tr.handler.count) != len(expected) {
+		tr.t.Errorf("count: length mismatch %v %v", tr.handler.count, expected)
 	}
 
 	for err, count := range expected {
@@ -331,22 +289,245 @@ func checkCount(t *testing.T, handler *dedupingErrorHandler, expected map[error]
 			errType: reflect.TypeOf(err),
 			message: err.Error(),
 		}
-		val, ok := handler.count[key]
+		val, ok := tr.handler.count[key]
 		if !ok {
-			t.Errorf("missing key %#v in %v", err, handler.count)
+			tr.t.Errorf("count: missing key %#v in %v", err, tr.handler.count)
 			continue
 		}
 		if val.count != uint64(count) {
-			t.Errorf("key %#v expected count %d got %d", err, count, val.count)
+			tr.t.Errorf("count: key %#v expected count %d got %d", err, count, val.count)
 		}
 	}
 }
 
-func checkCache(t *testing.T, handler *dedupingErrorHandler, expected int) {
-	t.Helper()
+func (tr *dedupingErrorHandlerTestRunner) checkCache(expected int) {
+	tr.t.Helper()
 
 	// TODO is it possible to do any check other than Len against the cache that does not mutate it?
-	if actual := handler.cache.Len(); actual != expected {
-		t.Errorf("expected cache length %d got %d", expected, actual)
+	if actual := tr.handler.cache.Len(); actual != expected {
+		tr.t.Errorf("cache: expected length %d got %d", expected, actual)
 	}
+}
+
+func (tr *dedupingErrorHandlerTestRunner) checkLog(called int) {
+	tr.t.Helper()
+
+	if tr.tracker.called != called {
+		tr.t.Errorf("log: expected %d got %d", called, tr.tracker.called)
+	}
+}
+
+func TestDedupingErrorHandler(t *testing.T) {
+	for _, tc := range []struct {
+		name  string
+		check func(r *dedupingErrorHandlerTestRunner)
+	}{
+		{
+			name: "simple unique errors",
+			check: func(r *dedupingErrorHandlerTestRunner) {
+				err1 := fmt.Errorf("1")
+				err2 := fmt.Errorf("2")
+				err3 := fmt.Errorf("3")
+
+				r.handler.handleErr(err1)
+				r.handler.handleErr(err2)
+				r.handler.handleErr(err3)
+
+				r.checkState(
+					map[error]int{
+						err1: 1,
+						err2: 1,
+						err3: 1,
+					},
+					3,
+					3,
+				)
+			},
+		},
+		{
+			name: "same error string but different types",
+			check: func(r *dedupingErrorHandlerTestRunner) {
+				err1 := fmt.Errorf("1")
+				err2 := stringErr("1")
+
+				r.handler.handleErr(err1)
+				r.handler.handleErr(err2)
+
+				r.checkState(
+					map[error]int{
+						err1: 1,
+						err2: 1,
+					},
+					2,
+					2,
+				)
+			},
+		},
+		{
+			name: "lru cache rollover to prevent infinite memory use",
+			check: func(r *dedupingErrorHandlerTestRunner) {
+				r.handler.cache.MaxEntries = 3
+
+				err1 := fmt.Errorf("1")
+				err2 := fmt.Errorf("2")
+				err3 := fmt.Errorf("3")
+				err4 := fmt.Errorf("4")
+
+				r.handler.handleErr(err1)
+				r.handler.handleErr(err2)
+				r.handler.handleErr(err3)
+				r.handler.handleErr(err4)
+
+				r.checkState(
+					map[error]int{
+						err2: 1,
+						err3: 1,
+						err4: 1,
+					},
+					3,
+					4,
+				)
+			},
+		},
+		{
+			name: "time based logging",
+			check: func(r *dedupingErrorHandlerTestRunner) {
+				err1 := fmt.Errorf("1")
+
+				// new error logs and increments counters
+				r.handler.handleErr(err1)
+				r.checkState(
+					map[error]int{
+						err1: 1,
+					},
+					1,
+					1,
+				)
+
+				// no time has passed but this is a power of 2 so we should log and increment counters
+				r.handler.handleErr(err1)
+				r.checkState(
+					map[error]int{
+						err1: 2,
+					},
+					1,
+					2,
+				)
+
+				// no time has passed and this is not a power of 2 so should not log, only increment counters
+				r.handler.handleErr(err1)
+				r.checkState(
+					map[error]int{
+						err1: 3,
+					},
+					1,
+					2,
+				)
+
+				// no time has passed but this is a power of 2 so we should log and increment counters
+				r.handler.handleErr(err1)
+				r.checkState(
+					map[error]int{
+						err1: 4,
+					},
+					1,
+					3,
+				)
+
+				// no time has passed and this is not a power of 2 so should not log, only increment counters
+				r.handler.handleErr(err1)
+				r.checkState(
+					map[error]int{
+						err1: 5,
+					},
+					1,
+					3,
+				)
+
+				// make some time pass
+				r.handler.clock.Sleep(r.handler.delta)
+
+				// time has passed so we should log and increment counters
+				r.handler.handleErr(err1)
+				r.checkState(
+					map[error]int{
+						err1: 6,
+					},
+					1,
+					4,
+				)
+			},
+		},
+		{
+			name: "similarity based deduplication",
+			check: func(r *dedupingErrorHandlerTestRunner) {
+				r.handler.Similar = 0.75
+
+				err1 := fmt.Errorf("testwithenoughsamedata1")
+				err2 := stringErr("testwithenoughsamedata1")
+				err3 := fmt.Errorf("testwithenoughsamedata2")
+				err4 := fmt.Errorf("testwithenoughsamedata3")
+
+				// new err
+				r.handler.handleErr(err1)
+				r.checkState(
+					map[error]int{
+						err1: 1,
+					},
+					1,
+					1,
+				)
+
+				// should not be the same as err1
+				r.handler.handleErr(err2)
+				r.checkState(
+					map[error]int{
+						err1: 1,
+						err2: 1,
+					},
+					2,
+					2,
+				)
+
+				// should be the same as err1, log due to power of 2
+				r.handler.handleErr(err3)
+				r.checkState(
+					map[error]int{
+						err1: 2,
+						err2: 1,
+					},
+					2,
+					3,
+				)
+
+				// should be the same as err1, do not log
+				r.handler.handleErr(err4)
+				r.checkState(
+					map[error]int{
+						err1: 3,
+						err2: 1,
+					},
+					2,
+					3,
+				)
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			// setup a test version of dedupingErrorHandler
+			d := newDedupingErrorHandler(10, 0, delta, 1)
+			d.clock = clock.NewFakeClock(time.Time{})
+			l := &logErrTracker{}
+			d.logErrorHandler = l.logErr
+			d.getStackHandler = func() (stack string) { return "" } // make all errors have the same stack
+			r := &dedupingErrorHandlerTestRunner{t: t, handler: d, tracker: l}
+
+			// run the test and check results
+			tc.check(r)
+		})
+	}
+}
+
+func TestDedupingErrorHandlerGetStack(t *testing.T) {
+	// TODO
 }
