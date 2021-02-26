2f344f58ba4a3f8a473c8b79cc76015626363456
Monis Khan <i@monis.app>
2021-02-26 09:50:33 -0500

    wip006
    Signed-off-by: Monis Khan <mok@vmware.com>

--- plugin/pkg/admission/certificates/notafter/admission_test.go
+++ plugin/pkg/admission/certificates/notafter/admission_test.go
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package certificates
+package notafter
 
-// TODO: add integration test for notAfter field
+// TODO: add unit test for admission plugin
