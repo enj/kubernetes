617ea7bf59ed37c25e46d7c821b55cd7f028bac1
Monis Khan <i@monis.app>
2023-03-01 17:14:36 -0500

    fix aes
    Signed-off-by: Monis Khan <mok@microsoft.com>

--- staging/src/k8s.io/apiserver/pkg/storage/value/encrypt/aes/aes.go
+++ staging/src/k8s.io/apiserver/pkg/storage/value/encrypt/aes/aes.go
@@ -23,9 +23,11 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
+	"sync/atomic"
 
 	"k8s.io/apiserver/pkg/storage/value"
 )
@@ -45,6 +47,7 @@ import (
 // random nonces.
 type gcm struct {
 	block cipher.Block
+	nonce atomic.Uint64
 }
 
 // NewGCMTransformer takes the given block cipher and performs encryption and decryption on the given
@@ -66,20 +69,33 @@ func (t *gcm) TransformFromStorage(ctx context.Context, data []byte, dataCtx val
 	return result, false, err
 }
 
-func (t *gcm) TransformToStorage(ctx context.Context, data []byte, dataCtx value.Context) ([]byte, error) {
+func (t *gcm) TransformToStorage(ctx context.Context, data []byte, dataCtx value.Context) ([]byte, error) { // TODO unit test changes
 	aead, err := cipher.NewGCM(t.block)
 	if err != nil {
 		return nil, err
 	}
 	nonceSize := aead.NonceSize()
 	result := make([]byte, nonceSize+aead.Overhead()+len(data))
-	n, err := rand.Read(result[:nonceSize])
+
+	// we only need 8 bytes to store our 64 bit incrementing nonce
+	// instead of leaving the unused bytes as zeros, set those to random bits
+	// this mostly protects us from weird edge cases like a VM restore that rewinds our atomic counter
+	randNonceSize := nonceSize - 8
+
+	n, err := rand.Read(result[:randNonceSize])
 	if err != nil {
 		return nil, err
 	}
-	if n != nonceSize {
+	if n != randNonceSize {
 		return nil, fmt.Errorf("unable to read sufficient random bytes")
 	}
+
+	incrementingNonce := t.nonce.Add(1)
+	if incrementingNonce == 0 {
+		panic("aes-gcm detected nonce overflow") // this should never happen, and is unrecoverable if it does
+	}
+	binary.LittleEndian.PutUint64(result[randNonceSize:nonceSize], incrementingNonce)
+
 	cipherText := aead.Seal(result[nonceSize:nonceSize], result[:nonceSize], data, dataCtx.AuthenticatedData())
 	return result[:nonceSize+len(cipherText)], nil
 }
