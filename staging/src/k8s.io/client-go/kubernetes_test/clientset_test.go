17e32d8b9485377e879f7f374566f2fb81ad9938
Monis Khan <i@monis.app>
2024-06-05 09:00:02 -0400

    Add test to confirm default content type used by core client
    Signed-off-by: Monis Khan <mok@microsoft.com>

--- staging/src/k8s.io/client-go/kubernetes_test/clientset_test.go
+++ staging/src/k8s.io/client-go/kubernetes_test/clientset_test.go
@@ -18,11 +18,14 @@ package kubernetes_test
 
 import (
 	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	v1 "k8s.io/api/core/v1"
+	"github.com/google/go-cmp/cmp"
+
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -61,7 +64,7 @@ func TestClientUserAgent(t *testing.T) {
 			ts.Start()
 			defer ts.Close()
 
-			gv := v1.SchemeGroupVersion
+			gv := corev1.SchemeGroupVersion
 			config := &rest.Config{
 				Host: ts.URL,
 			}
@@ -85,4 +88,74 @@ func TestClientUserAgent(t *testing.T) {
 		})
 	}
 
+}
+
+func TestClientContentType(t *testing.T) {
+	tests := []struct {
+		name              string
+		contentType       string
+		expectContentType string
+		expectBody        string
+	}{
+		{
+			name:              "default",
+			contentType:       "",
+			expectContentType: "application/vnd.kubernetes.protobuf",
+			expectBody:        "k8s\x00\n\t\n\x02v1\x12\x03Pod\x12I\n\x17\n\asnorlax\x12\x00\x1a\x00\"\x00*\x002\x008\x00B\x00\x12\x1c\x1a\x002\x00B\x00J\x00R\x00X\x00`\x00h\x00\x82\x01\x00\x8a\x01\x00\x9a\x01\x00\xc2\x01\x00\x1a\x10\n\x00\x1a\x00\"\x00*\x002\x00J\x00Z\x00r\x00\x1a\x00\"\x00",
+		},
+		{
+			name:              "json",
+			contentType:       "application/json",
+			expectContentType: "application/json",
+			expectBody: `{"kind":"Pod","apiVersion":"v1","metadata":{"name":"snorlax","creationTimestamp":null},"spec":{"containers":null},"status":{}}
+`,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path != "/api/v1/namespaces/panda/pods" {
+					t.Fatalf("unexpected path: %q", r.URL.Path)
+				}
+
+				if got, want := r.Header.Get("content-type"), tc.expectContentType; got != want {
+					t.Errorf("unexpected content-type, got=%q, want=%q", got, want)
+				}
+
+				if r.Body == nil {
+					t.Fatal("request body is nil")
+				}
+				body, err := io.ReadAll(r.Body)
+				if err != nil {
+					t.Fatal(err)
+				}
+				_ = r.Body.Close()
+				if diff := cmp.Diff(tc.expectBody, string(body)); len(diff) > 0 {
+					t.Errorf("body diff (-want, +got):\n%s", diff)
+				}
+
+				w.Header().Set("Content-Type", "application/json")
+				_, _ = w.Write([]byte("{}"))
+			}))
+			ts.Start()
+			defer ts.Close()
+
+			config := &rest.Config{
+				Host:          ts.URL,
+				ContentConfig: rest.ContentConfig{ContentType: tc.contentType},
+			}
+
+			client, err := kubernetes.NewForConfig(config)
+			if err != nil {
+				t.Fatalf("failed to create REST client: %v", err)
+			}
+
+			_, err = client.CoreV1().Pods("panda").
+				Create(context.TODO(), &corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "snorlax"}}, metav1.CreateOptions{})
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
 }
