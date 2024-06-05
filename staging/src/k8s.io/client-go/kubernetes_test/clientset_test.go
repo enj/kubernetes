3a0495cb5a71c6c9869cffc0b55cc39ec85e1880
Monis Khan <i@monis.app>
2024-06-05 09:00:02 -0400

    wip001
    Signed-off-by: Monis Khan <mok@microsoft.com>

--- staging/src/k8s.io/client-go/kubernetes_test/clientset_test.go
+++ staging/src/k8s.io/client-go/kubernetes_test/clientset_test.go
@@ -18,11 +18,13 @@ package kubernetes_test
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -61,7 +63,7 @@ func TestClientUserAgent(t *testing.T) {
 			ts.Start()
 			defer ts.Close()
 
-			gv := v1.SchemeGroupVersion
+			gv := corev1.SchemeGroupVersion
 			config := &rest.Config{
 				Host: ts.URL,
 			}
@@ -79,6 +81,54 @@ func TestClientUserAgent(t *testing.T) {
 				t.Error(err)
 			}
 			_, err = client.CoreV1().Secrets("").List(context.TODO(), metav1.ListOptions{})
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
+
+}
+
+func TestClientContentType(t *testing.T) {
+	tests := []struct {
+		name        string
+		contentType string
+		expectBody  string
+	}{
+		{
+			name:        "default",
+			contentType: "",
+			expectBody:  "k8s\x00\n\t\n\x02v1\x12\x03Pod\x12I\n\x17\n\asnorlax\x12\x00\x1a\x00\"\x00*\x002\x008\x00B\x00\x12\x1c\x1a\x002\x00B\x00J\x00R\x00X\x00`\x00h\x00\x82\x01\x00\x8a\x01\x00\x9a\x01\x00\xc2\x01\x00\x1a\x10\n\x00\x1a\x00\"\x00*\x002\x00J\x00Z\x00r\x00\x1a\x00\"\x00",
+		},
+		{
+			name:        "json",
+			contentType: "application/json",
+			expectBody:  `{"kind":"Pod","apiVersion":"v1","metadata":{"name":"snorlax","creationTimestamp":null},"spec":{"containers":null},"status":{}}`,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				body, _ := io.ReadAll(r.Body)
+				t.Error(fmt.Sprintf("%q", string(body)))
+				w.Header().Set("Content-Type", "application/json")
+				w.Write([]byte("{}"))
+			}))
+			ts.Start()
+			defer ts.Close()
+
+			config := &rest.Config{
+				Host:          ts.URL,
+				ContentConfig: rest.ContentConfig{ContentType: tc.contentType},
+			}
+
+			client, err := kubernetes.NewForConfig(config)
+			if err != nil {
+				t.Fatalf("failed to create REST client: %v", err)
+			}
+			_, err = client.CoreV1().Pods("panda").
+				Create(context.TODO(), &corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "snorlax"}}, metav1.CreateOptions{})
 			if err != nil {
 				t.Error(err)
 			}
